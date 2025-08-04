c9d635667d89b0af2438813f3c22f90fb4e605e0
Alexdev-sudo <ezequielcastm123@gmail.com>
2025-08-03 21:05:54 -0600

    Primer commit - revisando codigo control

---
+++ src/main/java/Codigo_Control_Colegio.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Codigo_Control_Colegio {
+    public static void main(String[]args){
+    Scanner leer = new Scanner(System.in);
+    System.out.println("Ingrese el codigo del colegio");
+
+    }
+}
